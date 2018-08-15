f7de72ed62caa88423f56218cd5e02fa560373a0
Mateusz <chuckstermajster@gmail.com>
2018-08-15 18:49:30 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import App from './components/index'
+ReactDOM.render(<App/>, document.getElementById('app'));
