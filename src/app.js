7dc2cea642f8f410785f227397eda43224f8f516
Mateusz <chuckstermajster@gmail.com>
2018-08-19 19:18:49 +0200

    setting up the header

--- src/app.js
+++ src/app.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import App from './components/index'
+
 ReactDOM.render(<App/>, document.getElementById('app'));
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import Header from './header';
 
 class App extends React.Component {
-   render() {
-       return <p>dsdsdccdscsd</p>
-   }
+    render() {
+        return (
+            <div>
+                <Header></Header>
+            </div>
+        )
+    }
 };
 
 export default App;
